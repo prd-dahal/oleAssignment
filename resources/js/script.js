f4c7479b2687277b78937ee32768a03bb438a708
prd-dahal <pradeep.dahal222@gmail.com>
2021-01-13 12:36:34 +0545

    functionality in experience

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,18 +1,15 @@
 experiences=[
-  {post:"[Junior Developer]",institute:"[St.Xavier's College]",workedOn:"[Student Management System]", date:"[September 2019 - Running]"},
-  {post:"[Intern]", institute:"[OLE Nepal]", workedOn:"[Epaath]",date:"[June 2017 - Sept 2017]" },
-  {post:"[President]", institute:"[Ekta Alumni Association]",workedOn:"",date:"[October 2018 - Till Date]"},
-  {post:"[Executive Member]", institute:"[Leo Club of Kathmandu Uttarbahini]",workedOn:"",date:"[August 2018 - Sept 2019]"}
+  {post:"Junior Developer",details:"I have worked as a junior developer at St.Xaviers' college on the project Student Management System from September 2019 to till date."},
+  {post:"Intern",details:"I have worked as an intern at OLE Nepal from June 2017 to Sept 2017 where I got the oppertunity to learn a work of an QA and several other networking things that were implemented by the organization" },
+  {post:"President", details:"I have been contributing to Ekta Alumni Association as a President from the date October 2018 to till date"},
 ]
 
 $(document).ready(function(){
   // use of loop to load the DOM contents
     $.each(experiences, function(number,experience){
       var post = experience.post;
-      var institute = experience.institute;
-      var workedOn = experience.workedOn;
-      var date = experience.date;
-      var h = "<p class='post subheading'>"+post+"</p>"+"<p class='institute'>"+ institute+"</p>" +"<p class='workedOn'>"+ workedOn+"</p>" +"<p class='expdate'>" + date + "</p>"
+      var details= experience.details;
+      var h = "<p class='experience_post'><i class='fas fa-angle-down expButton'></i>"+ post+"</p>"+"<p class='experience_details'>"+details+"</p>"
       console.log(h)
       $(".experiences").append("<div class='experience'>"+h+"</div>")
 
@@ -30,7 +27,17 @@ $(document).ready(function(){
           button.addClass("fa-angle-down")
         }
       });
-
-
+    })
+    $(".expButton").click(function(){
+      $(this).parent().siblings().slideToggle(400,function(){
+        var button = $(this).siblings().children()
+        if(button.hasClass("fa-angle-down")){
+          button.removeClass("fa-angle-down");
+          button.addClass("fa-angle-right");
+        }else{
+          button.removeClass("fa-angle-right");
+          button.addClass("fa-angle-down")
+        }
+      })
     })
 })
